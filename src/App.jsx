720adc46633ddc8938bdc878c3ad7c0803e5356f
Ian Pinili <gianpinili@gmail.com>
2024-04-23 22:37:38 -0700

    clean code comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Container from "./components/Container";
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Container />} />
-        </Routes>
-      </BrowserRouter> */}
       <Header />
       <Container />
     </>
